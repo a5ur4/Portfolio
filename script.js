576e3d363342587e10981fe58d9bf68ae5c8dbf0
_a5ur4 <113993134+a5ur4@users.noreply.github.com>
2022-11-28 21:09:44 -0300

    JAVASCRIPT DO SITE

---
+++ script.js
@@ -0,0 +1,24 @@
+function scroll_home() {
+    window.scrollTo(0, 400);
+}
+
+function scroll_projects() {
+    window.scrollTo(0, 1750)
+}
+
+function scroll_services() {
+    window.scrollTo(0, 2300)
+}
+
+function scroll_titles() {
+    window.scrollTo(0, 2950)
+}
+
+function scroll_contact() {
+    window.scrollTo(0, 3700)
+}
+
+function alert_login() {
+    var id = document.getElementById("nome").value;
+    alert("Uma mensagem foi enviada para seu email " + id);
+}
