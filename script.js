99c5c0bcfbbb1bc6b26b4bd84d3d85d0e20c4373
_a5ur4 <113993134+a5ur4@users.noreply.github.com>
2022-12-01 14:21:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 function scroll_home() {
-    window.scrollTo(0, 400);
+    window.scrollTo(0, 400)
+}
+
+function scroll_courses() {
+    window.scrollTo(0, 700)
 }
 
 function scroll_projects() {
-    window.scrollTo(0, 1750)
+    window.scrollTo(0, 2300)
 }
 
 function scroll_services() {
-    window.scrollTo(0, 2350)
+    window.scrollTo(0, 2870)
 }
 
 function scroll_titles() {
-    window.scrollTo(0, 3000)
+    window.scrollTo(0, 3550)
 }
 
 function scroll_contact() {
-    window.scrollTo(0, 3700)
+    window.scrollTo(0, 4230)
 }
 
 function alert_login() {
-    var id = document.getElementById("nome").value;
-    alert("Uma mensagem foi enviada para seu email " + id);
+    var id = document.getElementById("nome").value
+    alert("Uma mensagem foi enviada para seu email " + id)
 }
