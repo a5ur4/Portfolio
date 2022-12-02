1dfca819d3654f263408edced1a9ce545e039509
_a5ur4 <113993134+a5ur4@users.noreply.github.com>
2022-12-02 11:45:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
 function scroll_top() {
     window.scrollTo({
         top: 0,
-        behavior:"smooth",
-    })
-    
+        behavior: "smooth",
+    });
 }
 
 function scroll_home() {
-    window.scrollTo({ 
+    window.scrollTo({
         top: 400,
-        behavior:"smooth",
-    })
+        behavior: "smooth",
+    });
 }
 
 function scroll_courses() {
     window.scroll({
-        top:700,
-        behavior:"smooth",
-    })
+        top: 700,
+        behavior: "smooth",
+    });
 }
 
 function scroll_projects() {
     window.scroll({
-        top:2300,
-        behavior:"smooth",
-})
+        top: 2300,
+        behavior: "smooth",
+    });
 }
 
 function scroll_services() {
     window.scroll({
-        top:2870,
-        behavior:"smooth",
-    })
+        top: 2870,
+        behavior: "smooth",
+    });
 }
 
 function scroll_titles() {
     window.scroll({
-        top:3550,
-        behavior:"smooth",
-    })
+        top: 3550,
+        behavior: "smooth",
+    });
 }
 
 function scroll_contact() {
     window.scroll({
-        top:4230,
-        behavior:"smooth",
-    })
+        top: 4230,
+        behavior: "smooth",
+    });
 }
 
 function alert_login() {
-    var id = document.getElementById("nome").value
-    alert("Uma mensagem foi enviada para seu email " + id)
+    var id = document.getElementById("nome").value;
+    alert("Uma mensagem foi enviada para seu email " + id);
 }
